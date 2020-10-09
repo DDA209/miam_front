7b85615a66a87dd8449d3acf5c7d66ff9c07e7cf
DDA209 <didier.pascarel1@gmail.com>
2020-10-09 17:44:18 +0200

    refresh  09/10/2020 Didier

--- src/components/core/Utilisateur.js
+++ src/components/core/Utilisateur.js
@@ -1,40 +1,70 @@
 import React, {Component} from 'react';
-import  {Button, Form, FormControl, InputGroup} from 'react-bootstrap';
-import UserInfo from './UserInfo';
-import Quantities from './Quantities';
+import  {Button, Row, Col} from 'react-bootstrap';
+// import UserInfo from './UserInfo';
+// import Quantities from './Quantities';
 
 
 class Utilisateur extends Component {
-    state = {
-        form: true,
-    };
+    
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            userId: '5f806f7794e31a0b0045f236',
+            username: 'Batman',
+            recipes: []
+        };
 
+        this.processRecipe = this.processRecipe.bind(this);
+    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         form: true,
+    //         userId: '5f806f7794e31a0b0045f236',
+    //         username:'Batman',
+    //         recipes: []
+    //     };
+    // }
+
+    processRecipe (){
+
+        console.log('components/core/Utilisateur #processRecipe');
+        console.log('components/core/Utilisateur #processRecipe', this.state.userId);
+
+        const url = 'http://localhost:3003/recipes/users/' + this.state.userId;
+        // const url = 'http://localhost:3003/recipes/users/5f806f7794e31a0b0045f236';
+        fetch(url)
+        .then(res => res.json())
+        .then((json) => {
+            console.log(json.data);
+            this.setState({
+                recipes: json.data
+            })
+        })
+    }
 
     render (){
         return(
-            <div>               
 
-                <Form.Label>Nombre de personnes :</Form.Label>
-                <InputGroup className="mb-3">
-                    <FormControl
-                        placeholder="Nombre en chiffres"
-                        aria-label="Nombre en chiffres"
-                        aria-describedby="basic-addon2"
-                    />
-                    <InputGroup.Append>
-                        <Button variant="outline-secondary" type="submit">Personne(s)</Button>                    
-                    </InputGroup.Append>
-                </InputGroup>
-                
-                <Button variant="link" type="submit"><a href="/menu">Calculer le menu</a></Button>
+            <Row> 
+                <Col
+                    xs={{ span: 12, order: "first"}}
+                    className="mb-3"
+                >
+                </Col>         
+                <Col>
+                    
+                    <Button variant="outline-secondary" type="submit" onClick={this.processRecipe}>Calculer le menu</Button>
 
-                <hr /> 
+                    <hr /> 
 
-                <p>Ajouter d'ingrédients :</p>
-                <Button variant="outline-secondary"><a href="/add">Ajouter des ingrédients</a></Button>
-                        
+                    <p>Ajouter d'ingrédients :</p>
+                    <Button variant="outline-secondary"><a href="/add">Ajouter des ingrédients</a></Button>
+                    
+                </Col>
+            </Row> 
 
-            </div> 
         );
     }
 }
